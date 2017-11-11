115969a77a42482c7d69cddfe7c25cdc6d3cb917
zia196 <32339683+zia196@users.noreply.github.com>
2017-11-10 20:31:13 -0700

    Added out of order light

--- ca.ucalgary.seng300.a1/ca/ucalgary/seng300/a1/Controller.java
+++ ca.ucalgary.seng300.a1/ca/ucalgary/seng300/a1/Controller.java
@@ -1,4 +1,5 @@
-package ca.ucalgary.seng300.a2;
+package ca.ucalgary.seng300.ga2;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -50,9 +51,14 @@ public class Controller {
 	private MyButtonListener buttonListener;
 	private MyPopRackListener popRackListener;
 	private MyDeliveryChuteListener chuteListener;
-	private MyDisplayListener displayListener; //display listener 
+	private MyDisplayListener displayListener; //display listener
+	private MyOutOfOrderLightListener outOfOrderLightListener;
+	private MyExactChangeOnlyLightListener exactChangeOnlyLightListener;
+	private IndicatorLight outOfOrderLight;
+	private IndicatorLight exactChangeOnlyLight;
 	private static boolean coinSlotEnabled;
 	private static boolean buttonEnabled;
+	private static boolean outOfOrder;
 	Timer timer1 = new Timer(); //timers
 	Timer timer2 = new Timer();
 	
@@ -69,6 +75,9 @@ public class Controller {
 		//Canadian Currency, 6 types of pop, capacity of coinRack=15, 10 pops per rack, 200 coins in receptacle
 		client = new VendingMachine(CAD, 6, 15, 10, 200, 10, 10);	//10 is delivery chute capacity and coin return capacity - temp values because its not my problem
 
+		outOfOrderLight = client.getOutOfOrderLight();
+		exactChangeOnlyLight = client.getExactChangeLight();
+		
 		slotListener = new MySlotListener();
 		client.getCoinSlot().register(slotListener);
 
@@ -86,19 +95,41 @@ public class Controller {
 		chuteListener = new MyDeliveryChuteListener();
 		client.getDeliveryChute().register(chuteListener);
 		
+		outOfOrderLightListener = new MyOutOfOrderLightListener();
+		outOfOrderLight.register(outOfOrderLightListener);
+		
+		exactChangeOnlyLightListener = new MyExactChangeOnlyLightListener();
+		exactChangeOnlyLight.register(exactChangeOnlyLightListener);
+
 		
 		// set up the pop cans and prices and add them to the rack
 		List<String> popCanNames = new ArrayList<String>();
-		List<Integer> popCanCosts = new ArrayList<Integer>();
+		List<Integer> popCanCosts = new ArrayList<Integer>();	
 		
 		for (int i = 0; i<6; i++) {
 			popCanNames.add(Integer.toString(i));
 			popCanCosts.add(250);
-
 		}
-
-		client.configure(popCanNames, popCanCosts);
 		
+		//Create an array the size of total pop can options
+		int[] popCanAmounts = new int[popCanNames.size()];		
+		
+		//Populate this array with 5 in each index, for now we want 5 of each type of pop loaded into the machine
+		for(int i=0; i<popCanNames.size();i++)
+			popCanAmounts[i] = 5;			
+		
+		client.configure(popCanNames, popCanCosts);
+
+		//Load cans into the machine
+		client.loadPopCans(popCanAmounts);
+		
+		//If no cans have been loaded, toggle the outOfOrder light on
+		if(this.popCanRacksEmpty()) {
+			outOfOrder = true;
+			outOfOrderLight.activate();
+		}
+		
+				
 		//added code - this is the portion that changes to display while the credit value == 0
 		//every 15 second interval will have "Hi there" for 5 seconds then nothing for 10
 		//effective 5 sec display of hi followed by 10 seconds of blank
@@ -158,7 +189,23 @@ public class Controller {
 		return coinSlotEnabled;
 	}
 	
-	
+	/**
+	 * Checks if all pop can racks are empty
+	 * @return whether all pop can racks are empty or not
+	 */
+	public boolean popCanRacksEmpty() {
+		
+		boolean  rackWithPopExists = false;
+		
+		for(int i = 0; i < client.getNumberOfPopCanRacks(); i++)
+			if (client.getPopCanRack(i).size() > 0)
+				rackWithPopExists = true;
+		
+		if(rackWithPopExists)
+			return false;
+		else
+			return true;
+	}
 	/**
 	 * Used primarily for testing purposes of the listener
 	 * @return whether or not the button is enabled
@@ -174,7 +221,7 @@ public class Controller {
      *  If total becomes negative.
 	 */
 	public void decrementTotal(int price) throws SimulationException{
-		if (total>price) {
+		if (total >= price) {
 		total -= price;}
 		else throw new SimulationException("Decrement cannot result in total being a negative value");
 
@@ -214,23 +261,45 @@ public class Controller {
 			if (getTotal() >= client.getPopKindCost(button)){
 		  		
 		  		client.getSelectionButton(button).press();
+		  		
 		  		try {
+
+		  			try {
+		  				
+		  				//Store coins from CoinReceptacle into CoinRacks
+		  				client.getCoinReceptacle().storeCoins();
+		  			
+		  			} catch(CapacityExceededException e){		  				
+		  				//CoinRacks are full
+		  				outOfOrder = true;
+		  				outOfOrderLight.activate();
+		  				
+		  			} catch(DisabledException e) {throw new SimulationException("Coin Receptacle Disabled");}		  			
+					
 					client.getPopCanRack(button).dispensePopCan();
+					
 					decrementTotal(client.getPopKindCost(button));
+					
+					if(popCanRacksEmpty()) {
+						outOfOrder = true;
+						outOfOrderLight.activate();
+					}
+					
 				} catch (DisabledException e) {
 					// TODO Auto-generated catch block
 					throw new SimulationException("Pop rack is disabled");
 				} catch (EmptyException e) {
-					throw new SimulationException("Pop rack empty");
-					// TODO Auto-generated catch block
+					throw new SimulationException("Pop rack empty");					
 				
 				} catch (CapacityExceededException e) {
 					// TODO Auto-generated catch block
 					throw new SimulationException("Capacity exceeded");
 				}
 		  	}
-			else{
-				//Currently do nothing
+			else{ 
+				System.out.println("Not enough CREDIT!");
+				//Currently do nothing				
+
 			}
 		}
 		
@@ -412,7 +481,61 @@ public class Controller {
 			System.out.println("new message is ++\n" + newMessage); //for testing remove this when not needed 
 			messageBeingDisplayed = newMessage;						//for testing
 			
+		
 		}
 	}
+	private static class MyOutOfOrderLightListener implements IndicatorLightListener{
 
+		@Override
+		public void enabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
+			System.out.println("Out of Order Light enabled");
+			
+		}
+
+		@Override
+		public void disabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
+			System.out.println("Out of Order Light Disabled");
+			
+		}
+
+		@Override
+		public void activated(IndicatorLight light) {
+			System.out.println("Out of Order Light activated");
+			
+		}
+
+		@Override
+		public void deactivated(IndicatorLight light) {
+			System.out.println("Out of Order Light deactivated");
+			
+		}
+	}
+	private static class MyExactChangeOnlyLightListener implements IndicatorLightListener{
+
+		@Override
+		public void enabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
+			System.out.println("Exact Change Only Light enabled");
+			
+		}
+
+		@Override
+		public void disabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
+			System.out.println("Exact Change Only Light disabled");
+			
+		}
+
+		@Override
+		public void activated(IndicatorLight light) {
+			System.out.println("Exact Change Only Light activated");
+			
+		}
+
+		@Override
+		public void deactivated(IndicatorLight light) {
+			System.out.println("Exact Change Only Light deactivated");
+			
+		}
+		
+	}
+	
 }
